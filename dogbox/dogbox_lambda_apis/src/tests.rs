4dd816fce3082b63038575cb495d5532087ca4d4
TyRoXx <tyroxxxx@gmail.com>
2024-11-20 16:43:02 +0100

    WIP

---
+++ dogbox/dogbox_lambda_apis/src/tests.rs
@@ -0,0 +1,231 @@
+use crate::LoadedDirectory;
+use astraea::expressions::{evaluate, Application, Expression, LambdaExpression, Object, Pointer};
+use astraea::storage::{store_object, LoadValue};
+use astraea::tree::{BlobDigest, Reference};
+use astraea::type_checking::TypeCheckedExpression;
+use astraea::types::{Interface, Name, NamespaceId, Signature, Type, TypedExpression};
+use astraea::{
+    storage::{InMemoryValueStorage, StoreValue},
+    tree::{HashedValue, Value, ValueBlob},
+};
+use dogbox_tree::serialization::FileName;
+use dogbox_tree_editor::OpenDirectory;
+use std::collections::BTreeMap;
+use std::pin::Pin;
+use std::sync::Arc;
+
+#[tokio::test]
+async fn complex_expression() {
+    let storage = Arc::new(InMemoryValueStorage::empty());
+    let clock = || std::time::SystemTime::UNIX_EPOCH;
+    let directory = Arc::new(
+        OpenDirectory::create_directory(storage.clone(), clock, 1)
+            .await
+            .unwrap(),
+    );
+    let empty_file_digest = storage
+        .store_value(&HashedValue::from(Arc::new(Value::new(
+            ValueBlob::try_from(bytes::Bytes::new()).unwrap(),
+            vec![],
+        ))))
+        .await
+        .unwrap();
+    let file_name = "test.txt";
+    let open_file = directory
+        .clone()
+        .open_file(file_name, &empty_file_digest.digest)
+        .await
+        .unwrap();
+    let write_permission = open_file.get_write_permission();
+    let file_content = bytes::Bytes::from_static("Hello".as_bytes());
+    open_file
+        .write_bytes(&write_permission, 0, file_content.clone())
+        .await
+        .unwrap();
+    open_file.flush().await.unwrap();
+    drop(open_file);
+    let directory_status = directory.request_save().await.unwrap();
+    assert!(directory_status.digest.is_digest_up_to_date);
+
+    let namespace = NamespaceId([42; 16]);
+    let directory_type_name = Name::new(namespace, "directory".to_string());
+    let directory_type = Type::Named(directory_type_name);
+    let lambda_parameter_name = Name::new(namespace.clone(), "arg".to_string());
+    let read_lambda_parameter_expression = TypedExpression::new(
+        Expression::ReadVariable(lambda_parameter_name.clone()),
+        directory_type.clone(),
+    );
+    let file_name_type_name = Name::new(namespace, "file_name".to_string());
+    let file_name_type = Type::Named(file_name_type_name);
+    let regular_file_type_name = Name::new(namespace, "regular_file".to_string());
+    let regular_file_type = Type::Named(regular_file_type_name.clone());
+    let regular_file_type_ref = store_object(&*storage, &regular_file_type).await.unwrap();
+    let get_name = Name::new(namespace, "get".to_string());
+    let directory_interface = Arc::new(Interface::new(BTreeMap::from([(
+        get_name.clone(),
+        Signature::new(
+            file_name_type.clone(),
+            Type::Option(regular_file_type_ref.digest),
+        ),
+    )])));
+    let directory_interface_ref = store_object(&*storage, &*directory_interface)
+        .await
+        .unwrap();
+    let stored_file_name = storage
+        .store_value(&HashedValue::from(Arc::new(
+            Value::from_object(&FileName::try_from(file_name.to_string()).unwrap()).unwrap(),
+        )))
+        .await
+        .unwrap();
+    let get_expression = read_lambda_parameter_expression
+        .apply(
+            &directory_interface,
+            &directory_interface_ref.digest,
+            get_name.clone(),
+            TypedExpression::new(
+                Expression::Literal(file_name_type.clone(), stored_file_name.digest),
+                file_name_type.clone(),
+            ),
+        )
+        .unwrap();
+    let bytes_type_name = Name::new(namespace, "bytes".to_string());
+    let bytes_type = Type::Named(bytes_type_name);
+    let read_name = Name::new(namespace, "read".to_string());
+    let regular_file_interface = Interface::new(BTreeMap::from([(
+        read_name.clone(),
+        Signature::new(Type::Unit, bytes_type.clone()),
+    )]));
+    let regular_file_interface_ref = store_object(&*storage, &regular_file_interface)
+        .await
+        .unwrap();
+    let read_expression = get_expression
+        .apply(
+            &regular_file_interface,
+            &regular_file_interface_ref.digest,
+            read_name.clone(),
+            TypedExpression::unit(),
+        )
+        .unwrap();
+    let lambda_expression = TypedExpression::new(
+        Expression::Lambda(Box::new(LambdaExpression::new(
+            directory_type.clone(),
+            lambda_parameter_name.clone(),
+            read_expression.expression,
+        ))),
+        Type::Function(Box::new(Signature::new(
+            directory_type.clone(),
+            bytes_type.clone(),
+        ))),
+    );
+    let lambda_interface = Arc::new(Interface::new(BTreeMap::from([(
+        get_name.clone(),
+        Signature::new(
+            file_name_type.clone(),
+            Type::Option(regular_file_type_ref.digest),
+        ),
+    )])));
+    let lambda_interface_ref = store_object(&*storage, &*lambda_interface).await.unwrap();
+    let apply_name = Name::new(namespace, "apply".to_string());
+    let external_parameter_name = Name::new(namespace, "external".to_string());
+    let lambda_application = TypedExpression::new(
+        Expression::Apply(Box::new(Application::new(
+            lambda_expression.expression,
+            lambda_interface_ref.digest,
+            apply_name,
+            Expression::ReadVariable(external_parameter_name.clone()),
+        ))),
+        bytes_type,
+    );
+
+    /*
+    {
+        let find_variable = {
+            let directory_type = directory_type.clone();
+            let external_parameter_name = external_parameter_name.clone();
+            move |name: &Name| -> Option<Type> {
+                if name == &external_parameter_name {
+                    let directory_type = directory_type.clone();
+                    Some(directory_type)
+                } else {
+                    todo!()
+                }
+            }
+        };
+        let find_interface = {
+            let directory_type = directory_type.clone();
+            let directory_interface_ref = directory_interface_ref.clone();
+            let directory_interface = directory_interface.clone();
+            move         |digest: &BlobDigest,
+                                callee: Arc<Type>|
+            -> Pin<
+                Box<dyn core::future::Future<Output = Option<Arc<Interface>>> + Send>,
+            > {
+                if &directory_type == &*callee  {
+                    assert_eq!(&directory_interface_ref.digest, digest);
+                    Box::pin(core::future::ready(Some(directory_interface.clone())))
+                } else {
+                    assert_eq!(&regular_file_type,  &*callee);
+                    assert_eq!(&regular_file_interface_ref.digest, digest);
+                    todo!()
+                }
+            }
+        };
+        let checked =
+            TypeCheckedExpression::check(&lambda_application, &find_variable, &find_interface)
+                .await;
+        assert_eq!(
+            Ok(&lambda_application),
+            checked.as_ref().map(|success| success.correct())
+        );
+    }
+
+    let external_argument = {
+        let loaded = storage
+            .load_value(&Reference::new(directory_status.digest.last_known_digest))
+            .await;
+        match loaded {
+            Some(found) => match found.hash() {
+                Some(hashed) => {
+                    let parsed_directory: dogbox_tree::serialization::DirectoryTree =
+                        match postcard::from_bytes::<dogbox_tree::serialization::DirectoryTree>(
+                            hashed.value().blob().as_slice(),
+                        ) {
+                            Ok(success) => success,
+                            Err(_) => todo!(),
+                        };
+                    let hydrated: Arc<(dyn Object + Sync)> = Arc::new(LoadedDirectory::new(
+                        hashed.value().clone(),
+                        parsed_directory,
+                        directory_interface_ref.digest.clone(),
+                        get_name.clone(),
+                        regular_file_interface_ref.digest.clone(),
+                        read_name.clone(),
+                        storage.clone(),
+                    ));
+                    Pointer::Object(hydrated)
+                }
+                None => todo!(),
+            },
+            None => todo!(),
+        }
+    };
+    let read_variable =
+        move |name: &Name| -> Pin<Box<dyn core::future::Future<Output = Pointer> + Send>> {
+            if name == &external_parameter_name {
+                let external_argument = external_argument.clone();
+                Box::pin(async move { external_argument })
+            } else {
+                todo!()
+            }
+        };
+    let evaluation_result = evaluate(&lambda_application.expression, &*storage, &read_variable)
+        .await
+        .serialize(&*storage)
+        .await
+        .unwrap();
+    assert_eq!(
+        &Value::new(ValueBlob::try_from(file_content).unwrap(), Vec::new()),
+        &**evaluation_result.value()
+    );
+    */
+}
