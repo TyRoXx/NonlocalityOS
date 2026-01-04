b6ba96a08a6a096415558634aa8b0b7e83e304d2
TyRoXx <tyroxxxx@gmail.com>
2026-01-04 12:00:38 +0100

    test reopen

--- dogbox/dogbox_tree_editor/src/sqlite_tests.rs
+++ dogbox/dogbox_tree_editor/src/sqlite_tests.rs
@@ -91,6 +91,109 @@ async fn test_open_database() {
 }
 
 #[test_log::test(tokio::test)]
+async fn test_reopen_database() {
+    let storage = Arc::new(InMemoryTreeStorage::empty());
+    let clock = Arc::new(|| std::time::SystemTime::UNIX_EPOCH);
+    let directory = Arc::new(
+        OpenDirectory::create_directory(std::path::PathBuf::from(""), storage, clock, 1)
+            .await
+            .unwrap(),
+    );
+    let vfs_name = "test_vfs";
+    register_vfs(
+        vfs_name,
+        TreeEditor::new(directory.clone(), None),
+        tokio::runtime::Handle::current(),
+    )
+    .unwrap();
+    let insert_count = 100;
+    {
+        let thread = tokio::task::spawn_blocking(move || {
+            let connection = rusqlite::Connection::open_with_flags_and_vfs(
+                "test.db",
+                rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE
+                    | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
+                vfs_name,
+            )
+            .unwrap();
+            connection
+                .execute(
+                    "CREATE TABLE t (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL
+                ) STRICT",
+                    (),
+                )
+                .unwrap();
+            for i in 0..insert_count {
+                connection
+                    .execute("INSERT INTO t (name) VALUES (?1)", [format!("Name {}", i)])
+                    .unwrap();
+            }
+            connection.close().unwrap();
+        });
+        thread.await.unwrap();
+    }
+    {
+        let directory_status = directory.request_save().await.unwrap();
+        assert_eq!(directory_status.directories_open_count, 1);
+        assert_eq!(directory_status.directories_unsaved_count, 0);
+        assert_eq!(
+            &directory_status.open_files,
+            &OpenFileStats {
+                files_open_count: 1,
+                bytes_unflushed_count: 0,
+                files_open_for_reading_count: 1,
+                files_open_for_writing_count: 1,
+                files_unflushed_count: 0,
+            }
+        );
+    }
+    let mut entries = BTreeMap::new();
+    let mut entry_stream = directory.read().await;
+    while let Some(entry) = entry_stream.next().await {
+        match entry.kind {
+            crate::DirectoryEntryKind::File(size) => {
+                entries.insert(entry.name.clone(), size);
+            }
+            crate::DirectoryEntryKind::Directory => {
+                panic!("Unexpected directory");
+            }
+        }
+    }
+    assert_eq!(
+        &entries,
+        &BTreeMap::from([(FileName::try_from("test.db".to_string()).unwrap(), 8192)])
+    );
+    {
+        let thread = tokio::task::spawn_blocking(move || {
+            let connection = rusqlite::Connection::open_with_flags_and_vfs(
+                "test.db",
+                rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE
+                    | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
+                vfs_name,
+            )
+            .unwrap();
+            {
+                let mut select_statement = connection.prepare("SELECT id, name FROM t").unwrap();
+                let mut rows = select_statement.query([]).unwrap();
+                let mut count = 0;
+                while let Some(row) = rows.next().unwrap() {
+                    let id: i32 = row.get(0).unwrap();
+                    let name: String = row.get(1).unwrap();
+                    assert_eq!(id, count + 1);
+                    assert_eq!(name, format!("Name {}", count));
+                    count += 1;
+                }
+                assert_eq!(count, insert_count);
+            }
+            connection.close().unwrap();
+        });
+        thread.await.unwrap();
+    }
+}
+
+#[test_log::test(tokio::test)]
 async fn test_temp_table() {
     let storage = Arc::new(InMemoryTreeStorage::empty());
     let clock = Arc::new(|| std::time::SystemTime::UNIX_EPOCH);
