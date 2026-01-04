588fb7baf3cedc8fa8d073421b7c6e3e62b4198e
TyRoXx <tyroxxxx@gmail.com>
2026-01-04 12:04:19 +0100

    fix

--- dogbox/dogbox_tree_editor/src/sqlite.rs
+++ dogbox/dogbox_tree_editor/src/sqlite.rs
@@ -271,12 +271,11 @@ impl<const PAGE_SIZE: usize> sqlite_vfs::DatabaseHandle for DatabaseFile<PAGE_SI
     }
 
     fn set_chunk_size(&self, chunk_size: usize) -> Result<(), io::Error> {
-        if chunk_size != PAGE_SIZE {
-            eprintln!("set_chunk_size={chunk_size} (rejected)");
-            Err(io::Error::other("changing chunk size is not allowed"))
-        } else {
-            eprintln!("set_chunk_size={chunk_size}");
+        if chunk_size == PAGE_SIZE {
             Ok(())
+        } else {
+            error!("set_chunk_size={chunk_size} (rejected)");
+            Err(io::Error::other("changing chunk size is not allowed"))
         }
     }
 }
