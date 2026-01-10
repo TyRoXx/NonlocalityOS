bec11ea790dbc10e6c1e992487b3ba5efa5a34df
TyRoXx <tyroxxxx@gmail.com>
2026-01-10 08:10:34 +0100

    coverage

--- dogbox/dogbox_tree_editor/src/sqlite_tests.rs
+++ dogbox/dogbox_tree_editor/src/sqlite_tests.rs
@@ -1,10 +1,16 @@
-use crate::{sqlite::register_vfs, CacheDropStats, OpenDirectory, OpenFileStats, TreeEditor};
+use crate::{
+    sqlite::register_vfs, CacheDropStats, NormalizedPath, OpenDirectory, OpenFileStats, TreeEditor,
+};
 use astraea::storage::InMemoryTreeStorage;
 use dogbox_tree::serialization::FileName;
 use futures::StreamExt;
 use pretty_assertions::assert_eq;
 use rand::{rngs::SmallRng, SeedableRng};
-use std::{collections::BTreeMap, sync::Arc};
+use relative_path::RelativePath;
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    sync::Arc,
+};
 
 #[test_log::test(tokio::test)]
 async fn test_open_database() {
@@ -169,6 +175,86 @@ async fn test_open_invalid_file_name() {
 }
 
 #[test_log::test(tokio::test)]
+async fn test_open_failure() {
+    let storage = Arc::new(InMemoryTreeStorage::empty());
+    let clock = Arc::new(|| std::time::SystemTime::UNIX_EPOCH);
+    let directory = Arc::new(
+        OpenDirectory::create_directory(std::path::PathBuf::from(""), storage, clock, 1)
+            .await
+            .unwrap(),
+    );
+    let vfs_name = "test_vfs";
+    register_vfs(
+        vfs_name,
+        TreeEditor::new(directory.clone(), None),
+        tokio::runtime::Handle::current(),
+        Box::new(SmallRng::seed_from_u64(123)),
+    )
+    .unwrap();
+    let editor = TreeEditor::new(directory.clone(), None);
+    editor
+        .create_directory(NormalizedPath::try_from(RelativePath::new("/dir")).unwrap())
+        .await
+        .unwrap();
+    let thread = tokio::task::spawn_blocking(move || {
+        match rusqlite::Connection::open_with_flags_and_vfs(
+            "dir",
+            rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
+            vfs_name,
+        ) {
+            Ok(_) => panic!("Expected error"),
+            Err(e) => {
+                assert_eq!("unable to open database file: dir", e.to_string());
+            }
+        }
+    });
+    thread.await.unwrap();
+    {
+        let cache_drop_stats = directory.drop_all_read_caches().await;
+        assert_eq!(
+            &cache_drop_stats,
+            &CacheDropStats {
+                files_and_directories_remaining_open: 1,
+                hashed_trees_dropped: 0,
+                open_directories_closed: 0,
+                open_files_closed: 0,
+            }
+        );
+    }
+    {
+        let directory_status = directory.request_save().await.unwrap();
+        assert_eq!(directory_status.directories_open_count, 2);
+        assert_eq!(directory_status.directories_unsaved_count, 0);
+        assert_eq!(
+            &directory_status.open_files,
+            &OpenFileStats {
+                files_open_count: 0,
+                bytes_unflushed_count: 0,
+                files_open_for_reading_count: 0,
+                files_open_for_writing_count: 0,
+                files_unflushed_count: 0,
+            }
+        );
+    }
+    let mut entries = BTreeSet::new();
+    let mut entry_stream = directory.read().await;
+    while let Some(entry) = entry_stream.next().await {
+        match entry.kind {
+            crate::DirectoryEntryKind::File(_size) => {
+                panic!("Unexpected file");
+            }
+            crate::DirectoryEntryKind::Directory => {
+                entries.insert(entry.name.clone());
+            }
+        }
+    }
+    assert_eq!(
+        &entries,
+        &BTreeSet::from([FileName::try_from("dir".to_string()).unwrap()])
+    );
+}
+
+#[test_log::test(tokio::test)]
 async fn test_reopen_database() {
     let storage = Arc::new(InMemoryTreeStorage::empty());
     let clock = Arc::new(|| std::time::SystemTime::UNIX_EPOCH);
@@ -252,8 +338,8 @@ async fn test_reopen_database() {
         let thread = tokio::task::spawn_blocking(move || {
             let connection = rusqlite::Connection::open_with_flags_and_vfs(
                 "test.db",
-                rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE
-                    | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
+                // test read-only access
+                rusqlite::OpenFlags::SQLITE_OPEN_READ_ONLY,
                 vfs_name,
             )
             .unwrap();
