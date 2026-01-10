f61a39cad5f388684fcdced4d0bebae8288476dc
TyRoXx <tyroxxxx@gmail.com>
2026-01-10 08:05:30 +0100

    coverage

--- dogbox/dogbox_tree_editor/src/sqlite_tests.rs
+++ dogbox/dogbox_tree_editor/src/sqlite_tests.rs
@@ -97,6 +97,78 @@ async fn test_open_database() {
 }
 
 #[test_log::test(tokio::test)]
+async fn test_open_invalid_file_name() {
+    let storage = Arc::new(InMemoryTreeStorage::empty());
+    let clock = Arc::new(|| std::time::SystemTime::UNIX_EPOCH);
+    let directory = Arc::new(
+        OpenDirectory::create_directory(std::path::PathBuf::from(""), storage, clock, 1)
+            .await
+            .unwrap(),
+    );
+    let vfs_name = "test_vfs";
+    register_vfs(
+        vfs_name,
+        TreeEditor::new(directory.clone(), None),
+        tokio::runtime::Handle::current(),
+        Box::new(SmallRng::seed_from_u64(123)),
+    )
+    .unwrap();
+    let thread = tokio::task::spawn_blocking(move || {
+        match rusqlite::Connection::open_with_flags_and_vfs(
+            "\\",
+            rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
+            vfs_name,
+        ) {
+            Ok(_) => panic!("Expected error"),
+            Err(e) => {
+                assert_eq!("unable to open database file: \\", e.to_string());
+            }
+        }
+    });
+    thread.await.unwrap();
+    {
+        let cache_drop_stats = directory.drop_all_read_caches().await;
+        assert_eq!(
+            &cache_drop_stats,
+            &CacheDropStats {
+                files_and_directories_remaining_open: 1,
+                hashed_trees_dropped: 0,
+                open_directories_closed: 0,
+                open_files_closed: 0,
+            }
+        );
+    }
+    {
+        let directory_status = directory.request_save().await.unwrap();
+        assert_eq!(directory_status.directories_open_count, 1);
+        assert_eq!(directory_status.directories_unsaved_count, 0);
+        assert_eq!(
+            &directory_status.open_files,
+            &OpenFileStats {
+                files_open_count: 0,
+                bytes_unflushed_count: 0,
+                files_open_for_reading_count: 0,
+                files_open_for_writing_count: 0,
+                files_unflushed_count: 0,
+            }
+        );
+    }
+    let mut entries = BTreeMap::new();
+    let mut entry_stream = directory.read().await;
+    while let Some(entry) = entry_stream.next().await {
+        match entry.kind {
+            crate::DirectoryEntryKind::File(size) => {
+                entries.insert(entry.name.clone(), size);
+            }
+            crate::DirectoryEntryKind::Directory => {
+                panic!("Unexpected directory");
+            }
+        }
+    }
+    assert_eq!(&entries, &BTreeMap::from([]));
+}
+
+#[test_log::test(tokio::test)]
 async fn test_reopen_database() {
     let storage = Arc::new(InMemoryTreeStorage::empty());
     let clock = Arc::new(|| std::time::SystemTime::UNIX_EPOCH);
