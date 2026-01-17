b747477e7bcc93f233957218ae26dc6ab40b2d02
TyRoXx <tyroxxxx@gmail.com>
2026-01-17 09:46:15 +0100

    Print time and other resource stats for cargo tarpaulin because it's a bottleneck in our CI

--- coverage.sh
+++ coverage.sh
@@ -7,8 +7,12 @@ sccache --zero-stats || exit 1
 export CARGO_TARGET_DIR=target-coverage
 export RUST_BACKTRACE=1
 # https://crates.io/crates/cargo-tarpaulin
-echo "Running cargo tarpaulin"
-cargo tarpaulin --verbose --out lcov --out html --include-tests --ignore-panics --count --output-dir target-coverage --skip-clean --engine llvm --exclude-files 'target/*' || exit 1
+CARGO_TARPAULIN_COMMAND="/usr/bin/time -v cargo tarpaulin --verbose --out lcov --out html --include-tests --ignore-panics --count --output-dir target-coverage --skip-clean --engine llvm --exclude-files 'target/*'"
+
+echo "Building tests for cargo tarpaulin"
+eval "$CARGO_TARPAULIN_COMMAND --no-run" || exit 1
+echo "Running tests with cargo tarpaulin"
+eval "$CARGO_TARPAULIN_COMMAND" || exit 1
 
 echo "Showing sccache stats"
 sccache --show-stats || exit 1
