07805a2bf9b3d8cb77f0563c3c812367f058fe30
TyRoXx <tyroxxxx@gmail.com>
2026-01-24 10:29:10 +0100

    Fix formatting

--- astraea/src/delayed_hashed_tree_tests.rs
+++ astraea/src/delayed_hashed_tree_tests.rs
@@ -10,10 +10,11 @@ fn delayed_hashed_tree_inconsistent() {
     let tree_blob = TreeBlob::try_from(Bytes::from("test")).unwrap();
     let tree = Tree::new(tree_blob, TreeChildren::empty());
     let delayed_tree = DelayedHashedTree::delayed(
-       Arc::new(tree),
-        BlobDigest::parse_hex_string(
-            "f0140e314ee38d4472393680e7a72a81abb36b134b467d90ea943b7aa1ea03bf2323bc1a2df91f7230a225952e162f6629cf435e53404e9cdd727a2d94e4f909",
-        )
+        Arc::new(tree),
+        BlobDigest::parse_hex_string(concat!(
+            "f0140e314ee38d4472393680e7a72a81abb36b134b467d90ea943b7aa1ea03bf",
+            "2323bc1a2df91f7230a225952e162f6629cf435e53404e9cdd727a2d94e4f909"
+        ))
         .unwrap(),
     );
     assert!(delayed_tree.hash().is_none());
