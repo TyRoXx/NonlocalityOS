a5f89a057f8ff4ea4005c1f9989dc04fb9516985
TyRoXx <tyroxxxx@gmail.com>
2026-02-24 14:34:43 +0100

    Document how to run the tests on Windows

--- astra.bat
+++ astra.bat
@@ -1,5 +1,11 @@
 @echo off
 cls || exit /B 1
+
+rem Set LIBCLANG_PATH for any crates that need it during build
+if not defined LIBCLANG_PATH (
+    set "LIBCLANG_PATH=%~dp0tools\clang+llvm-22.1.0-x86_64-pc-windows-msvc\bin"
+)
+
 call .\scripts\install_sccache.bat || exit /B 1
 
 setlocal
