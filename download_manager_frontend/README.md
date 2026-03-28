e5ad4882c63241896b732b1e195ba278a626f127
mamazu <14860264+mamazu@users.noreply.github.com>
2026-03-28 12:35:50 +0100

    Adding documentation

---
+++ download_manager_frontend/README.md
@@ -0,0 +1,14 @@
+Tauri Docs: https://book.leptos.dev/
+
+Installation
+```bash
+cargo install
+```
+
+To run the webserver with hot reloading:
+```bash
+# Installing trunk (if not already installed)
+cargo install trunk
+
+trunk serve --port 3000 --open
+```
--- download_manager_frontend/src/main.rs
+++ download_manager_frontend/src/main.rs
@@ -26,34 +26,39 @@ fn add_video(url: String, set_state: WriteSignal<MyState>) {
     }
 }
 
+// https://book.leptos.dev/view/01_basic_component.html
 #[component]
 fn App() -> impl IntoView {
+    // Adding signal to track the text content of the input box
     let (text_value, set_text_value) = signal("".to_string());
 
-    // Load values from the local state
+    // Load values from the local storage key "my-state"
     let (state, set_state, _) = use_local_storage::<MyState, JsonSerdeCodec>("my-state");
 
     // Render the view
     return view! {
         <h1>Telegram Download Queue Manager</h1>
         <ul>
-            {move ||
+            {// We need a function here to force it to re-render on every call.
+            move ||
                 state.get()
                     .items
                     .into_iter()
-                    .map(|video: Video| view!{<li>
-                        {video.url} 
-                    </li>})
+                    .map(|video: Video| view!{<li> {video.url} </li>})
                     .collect_view()
             }
         </ul>
         <p>"Items in queue: " {move || state.get().items.len()}</p>
-        <form style="display: flex; gap: 1rem" 
+        <form style="display: flex; gap: 1rem"
             on:submit=move |ev| {
+                // Preventing the default browser submit (with page reload)
                 ev.prevent_default();
 
                 let value = text_value.get();
                 add_video(value, set_state);
+
+                // Set new value (independent of old value)
+                // If you want to have the new value be depending on the old one use `update`. See line 25
                 set_text_value.set(String::new());
             }
         >
@@ -63,9 +68,9 @@ fn App() -> impl IntoView {
                 prop:value={text_value}
                 required
             />
-            <button
-                type="submit"
-            >
+            <button type="submit">
+                // For some reason we need a "string" here the docs also can't really explain it
+                // For rendering a rust value use: {value}
                 "Download"
             </button>
         </form>
@@ -73,5 +78,9 @@ fn App() -> impl IntoView {
 }
 
 fn main() {
+    // Adding a console hook to show rust stack trace in browser
+    console_error_panic_hook::set_once();
+
+    // Mounting the root component of the app.
     leptos::mount::mount_to_body(App)
 }
