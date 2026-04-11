8c4e89d6ed0eca03d0a9b12f7f76b4e41dfecc97
TyRoXx <tyroxxxx@gmail.com>
2026-04-11 10:07:17 +0200

    Fix Windows encoding issue in yt-dlp; improve logging

--- download_manager/src/yt_dlp.rs
+++ download_manager/src/yt_dlp.rs
@@ -25,36 +25,19 @@ async fn update_yt_dlp(
     // nightly for the latest compatibility fixes
     cmd.arg("--update-to");
     cmd.arg("nightly");
-    cmd.stdout(Stdio::piped());
-    let mut child = cmd.spawn()?;
-    let stdout = child
-        .stdout
-        .take()
-        .expect("child did not have a handle to stdout");
-    let mut reader = BufReader::new(stdout).lines();
-    let (status_result, read_result): (std::io::Result<ExitStatus>, std::io::Result<()>) =
-        tokio::join!(child.wait(), async move {
-            while let Some(line) = reader.next_line().await? {
-                info!("yt-dlp: {}", line);
-            }
-            Ok(())
-        });
-    let status = status_result?;
-    info!("Child status was: {}", status);
-    if status.success() {
+    if run_and_log_command(cmd).await? {
         info!("yt-dlp updated successfully");
     } else {
-        let message = format!("yt-dlp exited with {status}");
+        let message = "yt-dlp update failed".to_string();
         error!("{message}");
         return Err(Box::from(message));
     }
-    read_result?;
     Ok(())
 }
 
 async fn install_deno() -> Result<(), Box<dyn std::error::Error>> {
     // https://docs.deno.com/runtime/getting_started/installation/
-    let mut cmd = if cfg!(windows) {
+    let cmd = if cfg!(windows) {
         let mut cmd = Command::new("powershell");
         cmd.arg("-Command");
         cmd.arg("irm https://deno.land/install.ps1 | iex");
@@ -65,60 +48,26 @@ async fn install_deno() -> Result<(), Box<dyn std::error::Error>> {
         cmd.arg("curl -fsSL https://deno.land/install.sh | sh");
         cmd
     };
-    cmd.stdout(Stdio::piped());
-    let mut child = cmd.spawn()?;
-    let stdout = child
-        .stdout
-        .take()
-        .expect("child did not have a handle to stdout");
-    let mut reader = BufReader::new(stdout).lines();
-    let (status_result, read_result): (std::io::Result<ExitStatus>, std::io::Result<()>) =
-        tokio::join!(child.wait(), async move {
-            while let Some(line) = reader.next_line().await? {
-                info!("deno: {}", line);
-            }
-            Ok(())
-        });
-    let status = status_result?;
-    info!("Child status was: {}", status);
-    if status.success() {
+    if run_and_log_command(cmd).await? {
         info!("deno installed successfully");
     } else {
-        let message = format!("deno installation exited with {status}");
+        let message = "deno installation failed".to_string();
         error!("{message}");
         return Err(Box::from(message));
     }
-    read_result?;
     Ok(())
 }
 
 async fn upgrade_deno(deno_bin: &std::path::Path) -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::new(deno_bin);
     cmd.arg("upgrade");
-    cmd.stdout(Stdio::piped());
-    let mut child = cmd.spawn()?;
-    let stdout = child
-        .stdout
-        .take()
-        .expect("child did not have a handle to stdout");
-    let mut reader = BufReader::new(stdout).lines();
-    let (status_result, read_result): (std::io::Result<ExitStatus>, std::io::Result<()>) =
-        tokio::join!(child.wait(), async move {
-            while let Some(line) = reader.next_line().await? {
-                info!("deno: {}", line);
-            }
-            Ok(())
-        });
-    let status = status_result?;
-    info!("Child status was: {}", status);
-    if status.success() {
+    if run_and_log_command(cmd).await? {
         info!("deno upgraded successfully");
     } else {
-        let message = format!("deno upgrade exited with {status}");
+        let message = "deno upgrade failed".to_string();
         error!("{message}");
         return Err(Box::from(message));
     }
-    read_result?;
     Ok(())
 }
 
@@ -167,6 +116,52 @@ pub fn hash_file(file_path: &std::path::Path) -> std::io::Result<BlobDigest> {
     Ok(BlobDigest::new(&digest_array))
 }
 
+async fn run_and_log_command(mut cmd: Command) -> Result<bool, std::io::Error> {
+    // Specify that we want the command's standard output piped back to us.
+    // By default, standard input/output/error will be inherited from the
+    // current process (for example, this means that standard input will
+    // come from the keyboard and standard output/error will go directly to
+    // the terminal if this process is invoked from the command line).
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+    let mut child = cmd.spawn()?;
+    let stdout = child
+        .stdout
+        .take()
+        .expect("child did not have a handle to stdout");
+    let stderr = child
+        .stderr
+        .take()
+        .expect("child did not have a handle to stderr");
+    let (status_result, stdout_result, stderr_result): (
+        std::io::Result<ExitStatus>,
+        std::io::Result<()>,
+        std::io::Result<()>,
+    ) = tokio::join!(
+        child.wait(),
+        async move {
+            let mut reader = BufReader::new(stdout).lines();
+            while let Some(line) = reader.next_line().await? {
+                info!("{}", line);
+            }
+            Ok(())
+        },
+        async move {
+            let mut reader = BufReader::new(stderr).lines();
+            while let Some(line) = reader.next_line().await? {
+                // yt-dlp uses stderr a lot for verbose/debug output
+                info!("{}", line);
+            }
+            Ok(())
+        }
+    );
+    let status = status_result?;
+    info!("Child status was: {}", status);
+    stdout_result?;
+    stderr_result?;
+    Ok(status.success())
+}
+
 async fn download_with_yt_dlp(
     executable_path: &std::path::Path,
     output_directory: &std::path::Path,
@@ -178,7 +173,7 @@ async fn download_with_yt_dlp(
     let temp_dir = tempfile::tempdir()?;
     let mut cmd = Command::new(executable_path);
     // sometimes useful for troubleshooting
-    // cmd.arg("--verbose");
+    cmd.arg("--verbose");
     cmd.arg("--no-overwrites");
     cmd.arg("--no-mtime");
     cmd.arg("--windows-filenames");
@@ -198,34 +193,23 @@ async fn download_with_yt_dlp(
     cmd.arg(format!("deno:{}", deno_bin.display()));
     cmd.arg(video_url);
 
-    // Specify that we want the command's standard output piped back to us.
-    // By default, standard input/output/error will be inherited from the
-    // current process (for example, this means that standard input will
-    // come from the keyboard and standard output/error will go directly to
-    // the terminal if this process is invoked from the command line).
-    cmd.stdout(Stdio::piped());
-    let mut child = cmd.spawn()?;
-    let stdout = child
-        .stdout
-        .take()
-        .expect("child did not have a handle to stdout");
-    let mut reader = BufReader::new(stdout).lines();
-    let (status_result, read_result): (std::io::Result<ExitStatus>, std::io::Result<()>) =
-        tokio::join!(child.wait(), async move {
-            while let Some(line) = reader.next_line().await? {
-                info!("yt-dlp: {}", line);
-            }
-            Ok(())
-        });
-    let status = status_result?;
-    info!("Child status was: {}", status);
-    if status.success() {
+    if run_and_log_command(cmd).await? {
         info!("yt-dlp completed successfully for URL: {}", video_url);
     } else {
-        error!("yt-dlp exited with {status} for URL: {}", video_url);
+        error!("yt-dlp failed for URL: {}", video_url);
+        #[cfg(windows)]
+        {
+            /* more complete error message:
+            ERROR: Unable to download video: [Errno 22] Invalid argument
+            Exception ignored in: <_io.TextIOWrapper name='<stdout>' mode='w' encoding='cp1252'>
+            OSError: [Errno 22] Invalid argument
+            */
+            info!(concat!(
+                "If you see an 'ERROR: Unable to download video: [Errno 22] Invalid argument' that also mentions 'cp1252', ",
+                "you may want to enable UTF-8 support: https://stackoverflow.com/a/57134096"));
+        }
         return Err(Box::from(format!("yt-dlp failed for URL: {}", video_url)));
     }
-    read_result?;
 
     let mut created_files = Vec::new();
     for entry_result in std::fs::read_dir(temp_dir.path())? {
